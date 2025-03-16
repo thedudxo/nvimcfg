a5db497ae2313eb8da204ed1c1f6c4e53d97a307
Isaac Boon <isaac@deltaStrike.com>
2025-03-16 13:40:43 +1300

    Initial commit

---
+++ lua/config/vimoptions.lua
@@ -0,0 +1,30 @@
+-- From lazy.nvim example:
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Full colour support
+vim.opt.termguicolors = true
+
+-- Line numbers
+vim.opt.number = true
+
+-- Minimal lines to keep above/below cursor
+vim.opt.scrolloff = 8
+
+-- 80 column indicator
+vim.opt.colorcolumn = '81'
+
+-- Tabs
+vim.opt.tabstop = 4       -- Tab size
+vim.opt.shiftwidth = 4    -- Autoindent spaces
+vim.opt.expandtab = true  -- Convert tabs to spaces
+
+-- Show whitespace
+vim.opt.list = true
+vim.opt.listchars = { trail = '◌', tab = '▸ '}
+-- Here is some whitespace for visualising:
+--			
+--                
