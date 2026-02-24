7f2fdad174b1f534a4c8c221ced925f98b5b9a88
DudV2 <isaacboon@gmail.com>
2026-02-25 09:21:39 +1300

    organise plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,80 @@
 return{
+
+    -- Theme
+    --{'folke/tokyonight.nvim', name = 'tokyonight' },
+    { 'nanotech/jellybeans.vim', name = 'jellybeans' },
+
     -- External tooling manager, can install things like LSP, Linter, etc
     {"williamboman/mason.nvim"},
+
+    -- Indentation guidelines
+    {"lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function()
+            require("ibl").setup({
+                indent = {char = '⁚'}
+            })
+        end
+    },
+
+    -- Telescope (file navigation)
+    {'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require("keymaps.telescope")
+            require("telescope").setup {
+                defaults = {
+                    path_display = {"filename_first"}
+                }
+            }
+      end
+    },
+
+    -- Keep the cursor in the center of the screen always
+    {'nullromo/go-up.nvim',
+        opts = {goUpLimit = 'center'},
+        config = function(_, opts)
+            local goUp = require('go-up')
+            goUp.setup(opts)
+
+            --Keep centered
+            vim.api.nvim_create_autocmd(
+                {"CursorMoved", "BufEnter"},{
+                pattern = "*",
+                callback = function()
+                    require('go-up').centerScreen();
+                end,
+            })
+
+            --Insert mode needs to respect cursor position more carefully
+            vim.api.nvim_create_autocmd("CursorMovedI", {
+                pattern = "*",
+                callback = function()
+                    local win = vim.api.nvim_get_current_win()
+                    local cursor = vim.api.nvim_win_get_cursor(win)
+                    require('go-up').centerScreen()
+                    vim.api.nvim_win_set_cursor(win, cursor)
+                end,
+            })
+        end,
+    },
+
+    -- Copilot
+    {'zbirenbaum/copilot.lua',
+        config = function()
+            require('copilot').setup({
+                suggestion = {
+                    auto_trigger = true,
+                    keymap = {
+                        accept_line = "<M-y>",
+                    },
+                },
+                filetypes = {
+                    markdown = true,
+                  },
+                })
+        end,
+    },
 
     -- C# LSP client
     {"OmniSharp/omnisharp-vim",
@@ -43,8 +117,18 @@ return{
         end
     },
 
+    -- Autocompletion
+    -- Error highligting
+    -- Outliner
+    -- intergrates with omnisharp
+    {'neoclide/coc.nvim',
+        branch = 'release',
+        ft = { 'cs', 'csproj', 'sln', 'slnx' },
+    },
+
     -- Powershell LSP
     {'TheLeoP/powershell.nvim',
+        ft = { 'ps1' },
         opts = {
             bundle_path = vim.fn.stdpath "data" .. '/mason/packages/powershell-editor-services'
         }
@@ -52,6 +136,7 @@ return{
 
     {'saghen/blink.cmp',
         dependencies = { 'rafamadriz/friendly-snippets' },
+        ft = { 'odin' },
         opts = {
             keymap = { preset = 'default' },
             appearance = {
@@ -67,115 +152,27 @@ return{
         dependencies = {
             'saghen/blink.cmp',
         },
+        ft = { 'odin' },
         config = function()
             local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
             local capabilities = require('blink.cmp').get_lsp_capabilities()
             vim.lsp.config('ols', {
                 cmd = { mason_path .. "/ols" },
-                --capabilities = capabilities
+                capabilities = capabilities
             })
         end
     },
 
     -- Debugger
-    --{'puremourning/vimspector',
-    --    init = function()
-    --        vim.g.vimspector_enable_mappings = 'HUMAN'
-    --    end
-    --},
-
-    -- Theme
-    --{'folke/tokyonight.nvim', name = 'tokyonight' },
-    { 'nanotech/jellybeans.vim', name = 'jellybeans' },
-
-    -- Telescope (file navigation)
-    {'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require("keymaps.telescope")
-            require("telescope").setup {
-                defaults = {
-                    path_display = {"filename_first"}
-                }
-            }
-      end
-    },
-
-    -- Indentation guidelines
-    {"lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        config = function()
-            require("ibl").setup({
-                indent = {char = '⁚'}
-            })
-        end
-    },
-
-    -- Autocompletion
-    -- Error highligting
-    -- Outliner
-    {'neoclide/coc.nvim',
-        branch = 'release',
-        config = function()
-            -- use control-L in Autocompletion menu to refresh it.
-            -- Useful for copilot suggestions, which come in late.
-            vim.keymap.set("i", "<C-l>", function()
-              return vim.fn["coc#refresh"]()
-            end, { silent = true, expr = true })
-        end
-    },
-
-    -- Copilot
-    {'zbirenbaum/copilot.lua',
-        config = function()
-            require('copilot').setup({
-                suggestion = {
-                    auto_trigger = true,
-                    keymap = {
-                        accept_line = "<M-y>",
-                    },
-                },
-            filetypes = {
-                markdown = true,
-              },
-            });
-        end,
+    {'puremourning/vimspector',
+         ft = { 'cs' },
+         init = function()
+             vim.g.vimspector_enable_mappings = 'HUMAN'
+         end
     },
 
     -- Snippets
-    -- Does not seem to be working well?
-    -- probably needs to intergrate with coc properly.
-    -- see :CocList sources
-    -- see https://github.com/neoclide/coc.nvim/wiki/Completion-with-sources
     -- TODO: TRY IT WITH blink! (wasnt there before)
     -- {'SirVer/ultisnips'},   -- Engine
     -- {'honza/vim-snippets'}, -- Snippets
-
-    -- Allow scrolling up above the top of the file
-    {'nullromo/go-up.nvim',
-        opts = {
-            --mapZZ = false,
-            --respectScrolloff = true,
-            goUpLimit = 'center'},
-        config = function(_, opts)
-            local goUp = require('go-up')
-            goUp.setup(opts)
-            vim.api.nvim_create_autocmd(
-                {"CursorMoved", "BufEnter"},{
-                pattern = "*",
-                callback = function()
-                    require('go-up').centerScreen();
-                end,
-            })
-            vim.api.nvim_create_autocmd("CursorMovedI", {
-                pattern = "*",
-                callback = function()
-                    local win = vim.api.nvim_get_current_win()
-                    local cursor = vim.api.nvim_win_get_cursor(win) -- {row, col}
-                    require('go-up').centerScreen()
-                    vim.api.nvim_win_set_cursor(win, cursor)
-                end,
-            })
-        end,
-    }
 }
