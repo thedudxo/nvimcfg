30086a6648c7c64776a583e96e33c971e16b8595
DudV2 <isaacboon@gmail.com>
2026-02-25 09:21:37 +1300

    Enable live grep telescope [fg]

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -2,9 +2,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {
     desc = 'Telescope find files' })
 
---requires ripgrep
---vim.keymap.set('n', '<leader>fg', builtin.live_grep, { 
---    desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { 
+    desc = 'Telescope live grep' })
 
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {
     desc = 'Telescope buffers' })
